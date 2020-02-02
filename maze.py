2f28947bbc69f9d35ae5c0391359695c7be7dd26
KaiWoonNP <s10182100@connect.np.edu.sg>
2020-02-02 12:15:03 +0800

    #6 Play maze feature

--- maze.py
+++ maze.py
@@ -11,6 +11,8 @@ import time
 from time import strptime
 import sys
 import os.path
+import copy
+
 
 #######################################D#######I########A#########N######### SelectionMenu ####I#############Z##############Z################A##############T#############I########
 #listing the menu in a list
@@ -22,6 +24,7 @@ SelectionMenu = ['Read and load maze from file',
 Maze = []
 dataInFile = []
 exitGame = False
+backToMenu = False
 
 def menusOfMaze(SelectionMenu):
     menuString = "\nMAIN MENU\n==========\n"
@@ -40,6 +43,55 @@ def displayMainMenu():
     mainMenuSelection(selection)
     if exitGame == False:
         displayMainMenu()
+
+def displayGamePlayMenu(playmaze):
+    outputString = ""
+    startLocation = ""
+    endLocation = ""
+    if (playmaze != []):
+        outputString = ViewMaze(playmaze)
+        row = 0
+        while row < len(playmaze):
+            col = 0
+            while col < len(playmaze[row]):
+                if (playmaze[row][col] == "A"):
+                    startLocation = "(Row " + str(row) + ", Column " + str(col) + ")"
+                if (playmaze[row][col] == "B"):
+                    endLocation = "(Row " + str(row) + ", Column " + str(col) + ")"
+                col += 1
+            row += 1
+            
+        outputString += "\nLocation of Start (A) = " + startLocation
+        outputString += "\nLocation of End (B) = " + endLocation + "\n"
+
+    else:
+        outputString = "\nPlease load a maze first!"
+
+    return outputString
+
+def playMazeMovementPrompt(playmaze):
+    gameMenuOutputString = displayGamePlayMenu(playmaze)
+    print(gameMenuOutputString)
+    if gameMenuOutputString != "\nPlease load a maze first!":
+        movementKey = str(input("Press 'W' for UP, 'A' for LEFT, 'S' for DOWN, 'D' for RIGHT, 'M' for MAIN MENU: "))
+        outputString, playmaze = movementKeySelection(playmaze, movementKey.upper())
+            
+    else:
+        displayMainMenu()
+
+    return outputString, playmaze
+
+def playMazeMovementPrompt(playMaze):
+    gameMenuOutputString = displayGamePlayMenu(playMaze)
+    print(gameMenuOutputString)
+    if gameMenuOutputString != "\nPlease load a maze first!":
+        movementKey = str(input("Press 'W' for UP, 'A' for LEFT, 'S' for DOWN, 'D' for RIGHT, 'M' for MAIN MENU: "))
+        outputString, playMaze = movementKeySelection(playMaze, movementKey.upper())
+            
+    else:
+        displayMainMenu()
+        
+    return outputString, playMaze
 
 def mainMenuSelection(selectedOption):
     if selectedOption == "1":
@@ -60,6 +112,14 @@ def mainMenuSelection(selectedOption):
     elif selectedOption == "3":
         print()
         print('Option 3: ' + SelectionMenu[2])
+        playMaze = copy.deepcopy(dataInFile)
+        global completedGame
+        completedGame = False
+        while completedGame == False:
+            outputString, playMaze = playMazeMovementPrompt(playMaze)
+            print(outputString)
+        if completedGame == True:
+            print("You have completed the maze successfully!")
 
     elif selectedOption == "4":
         print()
@@ -70,6 +130,64 @@ def mainMenuSelection(selectedOption):
     
     else:
         ErrorMessage()
+
+#######################################D#######I########A#########N######### GamePlay Menu ####I#############Z##############Z################A##############T#############I##############
+def movementKeySelection(playmaze, movementKey):
+    outputString = ""
+    if movementKey == "W" or movementKey == "A" or movementKey == "S" or movementKey == "D":
+        row = 0
+        startLocation = []
+        endLocation = []
+        moveLocation = []
+        while row < len(playmaze):
+            col = 0
+            while col < len(playmaze[row]):
+                if (playmaze[row][col] == "A"):
+                    startLocation = [row, col]
+                if (playmaze[row][col] == "B"):
+                    endLocation = [row, col]
+                col += 1
+            row += 1
+
+        direction = ""
+            
+        if movementKey == "W":
+            direction = "up"
+            moveLocation = [startLocation[0] - 1, startLocation[1]]
+        elif movementKey == "A":
+            direction = "left"
+            moveLocation = [startLocation[0], startLocation[1] - 1]
+        elif movementKey == "S":
+            direction = "down"
+            moveLocation = [startLocation[0] + 1, startLocation[1]]
+        elif movementKey == "D":
+            direction = "right"
+            moveLocation = [startLocation[0], startLocation[1] + 1]
+
+        if moveLocation[0] < 0 or moveLocation[0] > (len(playmaze)-1) or moveLocation[1] < 0 or moveLocation[1] > (len(playmaze[0])-1):
+            outputString = "\nInvalid movement. Please try again.\n"
+        else:
+            moveTo = playmaze[moveLocation[0]][moveLocation[1]]
+            
+            if moveTo == "X":
+                outputString = "\nInvalid movement. Please try again.\n"
+            elif moveTo == "B":
+                outputString = "\nYou have moved " + direction + ".\n"
+                playmaze[moveLocation[0]][moveLocation[1]] = "A"
+                playmaze[startLocation[0]][startLocation[1]] = "O"
+                global completedGame
+                completedGame = True
+            elif moveTo == "O":
+                outputString = "\nYou have moved " + direction + ".\n"
+                playmaze[moveLocation[0]][moveLocation[1]] = "A"
+                playmaze[startLocation[0]][startLocation[1]] = "O"
+            
+    elif movementKey == "M":
+        displayMainMenu()
+    else:
+         outputString = "\nInvalid input. Please try again.\n"
+
+    return outputString, playmaze
 
 #######################################D#######I########A#########N######### Option 1 ####I#############Z##############Z################A##############T#############I##############
 def ReadMazeInfoFromFile(fileName): 
